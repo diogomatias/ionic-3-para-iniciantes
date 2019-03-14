00341981f3ed921d74454c51456e0fa7dec6f7cc
Diogo Matias <diogo-mathias@live.com>
2019-03-14 01:40:15 -0300

    Second commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+  public nome_usuario: string = "Diogo Matias";
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumeros(num1: number, num2: number): void {
+    alert(num1 + num2);
+  }
+
+  ionViewDidLoad() {
+   // this.somaDoisNumeros(5, 15);
+  }
+
+}
